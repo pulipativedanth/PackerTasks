0ccf62d11b8eacb7bf0a1fb3963499bfcad7f7cc
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-11-05 19:53:41 +0530

    Update vm_create.tf

--- vm_create.tf
+++ vm_create.tf
@@ -7,7 +7,7 @@ provider "azurerm" {
 }
 
 variable "prefix" {
-  default = "PackerTestEU7"
+  default = "PackerTestEU8"
 }
 
 variable "resourcegroup" {
@@ -78,7 +78,7 @@ resource "azurerm_virtual_machine" "main" {
 	id				  = "/subscriptions/${var.subscription_id}/resourceGroups/${var.resourcegroup}/providers/Microsoft.Compute/images/${var.imagename}"
   }
   storage_os_disk {
-    name              = "myosdisk1"
+    name              = "myosdisk13"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
