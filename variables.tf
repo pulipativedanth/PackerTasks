82ceffa85482d5eade4b160b433c6b6f4e51a93f
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-11-07 22:56:34 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "prefix" {
+  default = "PackerTestEU10"
+}
+
+variable "resourcegroup" {
+	default = "saas-build-sig-01"
+}
+variable "location" {
+	default = "East Asia"
+}
+
+variable "subscription_id" {
+	default = "85dc525a-7861-44e9-a686-be41401bdd42"
+}
+variable "tenant_id" {
+	default = "00c023c5-6848-4fbe-bc02-abf3bf584bbe"
+}
+variable "client_secret" {
+	default = "JIFQFq7w-XSbv36pU6_lQWyDj2gM6MH1pa"
+}
+variable "client_id" {
+	default = "134bea01-5247-4bcf-bbc5-700f91a61ca6"
+}
+variable "imagename" {
+	default = "imageunique"
+}
