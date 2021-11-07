83289603ad495f5012dce311e69d4ca10f7fed30
pulipativedanth <80522589+pulipativedanth@users.noreply.github.com>
2021-11-07 22:48:51 +0530

    Update vm_create.tf

--- vm_create.tf
+++ vm_create.tf
@@ -1,11 +1,9 @@
 provider "azurerm" {
-	
-  	client_id = var.client_id
-    client_secret = var.client_secret
-    tenant_id = var.tenant_id
-    subscription_id = var. subscription_id
-    version = "=2.46.0"
     features {}
+	subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
 }
 
 variable "prefix" {
